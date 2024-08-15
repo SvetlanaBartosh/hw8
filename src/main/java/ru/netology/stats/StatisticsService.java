29e38e5e1c8311274ed5841c26ccff167c1ea3d5
Svetlana Bartosh <svetochk.sveta@yandex.ru>
2024-08-15 23:07:36 +0300

    Added test findMax2

--- src/main/java/ru/netology/stats/StatisticsService.java
+++ src/main/java/ru/netology/stats/StatisticsService.java
@@ -4,7 +4,7 @@ public class StatisticsService {
     public long findMax(long[] incomes) {
         long currentMax = incomes[0];
         for (long income : incomes) {
-            if (currentMax < income) {
+            if (currentMax <= income) {
                 currentMax = income;
             }
         }
